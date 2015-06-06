9b577019a4ce31ad0184e2e151a7b891ec3acb45
Ashesh Kumar <user501254@gmail.com>
2015-06-06 22:18:01 +0530

    fixes loop that counts number of fields

--- summary.php
+++ summary.php
@@ -60,8 +60,8 @@ if (!isset($_SESSION['username'])) {
       if (isset($_POST['report'])) {
         $user_course_table = $_POST['report'];
 
-        //count the number of feilds (empty and nonempty)
-        for ($i=0; $i<(count($_POST['start'])-1) || $i<(count($_POST['end'])-1) ; $i++){
+        //count the number of fields (empty and nonempty)
+        for ($i=0; $i<=(count($_POST['start'])-1) || $i<=(count($_POST['end'])-1) ; $i++){
           //if any start is not empty put in databse
           if(!empty($_POST['start'][$i])){
 
